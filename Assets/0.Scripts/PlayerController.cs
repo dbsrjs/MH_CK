fe6771baf5217acdd2797425c8271e118488b54e
boseung950 <dbsrjs1224@gmail.com>
2025-05-08 19:56:33 +0900

    [2025-05-08] Player animation

--- Assets/0.Scripts/PlayerController.cs
+++ Assets/0.Scripts/PlayerController.cs
@@ -6,29 +6,45 @@ public class PlayerController : MonoBehaviour
 {
     [Header("Component")]
     [SerializeField]
-    private Transform pla;
+    private Transform playerBody;
     [SerializeField]
     private Transform cameraArm;
+    [SerializeField]
+    private Rigidbody rb;
+
+    private Animator animator;
+
 
     private float speed = 5f;
+    private bool isMove = false;
+
+    private void Awake()
+    {
+        animator = playerBody.GetComponent<Animator>();
+    }
 
     void Update()
     {
         LookAround();
         Move();
+
+        if(Input.GetKeyDown(KeyCode.Space))
+            Jump();
     }
 
     private void Move()
     {
         Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        bool isMove = moveInput.magnitude != 0;
+        isMove = moveInput.magnitude != 0;
+        animator.SetBool("onMove", isMove);
+
         if (isMove)
         {
             Vector3 lookForward = new Vector3(cameraArm.forward.x, 0f, cameraArm.forward.z).normalized;
             Vector3 lookRight = new Vector3(cameraArm.right.x, 0f, cameraArm.right.z).normalized;
             Vector3 moveDir = lookForward * moveInput.y + lookRight * moveInput.x;
 
-            pla.forward = lookForward;
+            playerBody.forward = moveDir;
             transform.position += moveDir * Time.deltaTime * 5f;
         }
 
@@ -40,17 +56,24 @@ public class PlayerController : MonoBehaviour
         Vector2 mouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
         Vector3 camAngle = cameraArm.rotation.eulerAngles;
 
-        //추가
+        //각도 제한
         float x = camAngle.x - mouseDelta.y;
         if (x < 180f)
-        {
             x = Mathf.Clamp(x, -1f, 70f);
-        }
         else
-        {
             x = Mathf.Clamp(x, 335f, 361f);
-        }
 
         cameraArm.rotation = Quaternion.Euler(x, camAngle.y + mouseDelta.x, camAngle.z);
+    }
+
+    private void Jump()
+    {
+        if(animator.GetBool("onMove"))
+            animator.SetTrigger("isRunJump");
+
+        else
+            animator.SetTrigger("isJump");
+
+        rb.AddForce(Vector3.up * 4f, ForceMode.Impulse);
     }
 }
